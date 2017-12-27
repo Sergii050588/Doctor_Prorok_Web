b9cd8af5bfd6bbc665c889aab8a729332f85b5e9
Sergii050588 <mozgoved88@mail.ru>
2017-12-27 17:40:52 +0200

    dasda

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :repo_url, 'https://Sergii050588:g8s12kl9@github.com/Sergii050588/Doctor_Pro
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/Rails/"
+set :deploy_to, "/home/railman/Rails/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
